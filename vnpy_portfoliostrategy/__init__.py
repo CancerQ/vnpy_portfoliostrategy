a437b0a29c253f67f0a4411484e2ef5c20b1608f
zhiqiang_ye <zhiqiang_ye@foxitsoftware.com>
2024-08-06 11:36:43 +0800

    updaye v 1.10

--- vnpy_portfoliostrategy/__init__.py
+++ vnpy_portfoliostrategy/__init__.py
@@ -23,6 +23,7 @@
 
 from pathlib import Path
 
+import importlib_metadata
 from vnpy.trader.app import BaseApp
 from vnpy.trader.constant import Direction
 from vnpy.trader.object import TickData, BarData, TradeData, OrderData
@@ -32,6 +33,12 @@ from .base import APP_NAME
 from .engine import StrategyEngine
 from .template import StrategyTemplate
 from .backtesting import BacktestingEngine
+from .locale import _
+
+try:
+    __version__ = importlib_metadata.version("vnpy_portfoliostrategy")
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "dev"
 
 
 class PortfolioStrategyApp(BaseApp):
@@ -40,7 +47,7 @@ class PortfolioStrategyApp(BaseApp):
     app_name: str = APP_NAME
     app_module: str = __module__
     app_path: Path = Path(__file__).parent
-    display_name: str = "组合策略"
+    display_name: str = _("组合策略")
     engine_class: StrategyEngine = StrategyEngine
     widget_name: str = "PortfolioStrategyManager"
     icon_name: str = str(app_path.joinpath("ui", "strategy.ico"))
